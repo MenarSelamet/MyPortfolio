37f4b7e1222fd87263672b3f2d44a9c2087f836d
Menar Selamet <menarselamet007@gmail.com>
2023-02-28 00:30:33 +0300

    About section changed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,10 +8,18 @@ function About() {
         <div className="col-span-2">
           <h2 className="uppercase tracking-widest py-4">About</h2>
           <p className="py-2">
-            Detail-oriented Front-End Web Developer. With experience in React
-            and React-related libraries and a zest for creating dynamic,
-            user-friendly web applications. I possess advanced Agile Project
-            Management skills and am a motivated team player.
+            Hi, and welcome .. I am Menar, a frontend Web Developer
+            specializing in JavaScript frameworks and libraries and development
+            platforms for hosting databases with experience in building
+            single-page dynamic user-friendly web apps. While I come from a
+            different career background, I have always been immensely interested
+            in technology, and I find that in the world of coding, there's
+            always more to learn and more to gain, which is what makes this
+            field ever more interesting. I am on a continuous journey of growth
+            while obtaining more knowledge and gaining expertise that would be
+            an essence to my work, professionally and personally. Feel free to
+            get in touch with me if you feel like we can collaborate on a
+            project or maybe work together ..
           </p>
           <a
             href="https://www.canva.com/design/DAFSRmWYo_k/B69moS0ux5KGgfvRBzZvhA/view?utm_content=DAFSRmWYo_k&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
